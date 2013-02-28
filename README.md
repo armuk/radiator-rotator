72ea01c882a2679d43f435cf0956d3d327bac32d
Ricardo Gladwell <ricardo.gladwell@itv.com>
2013-02-28 18:18:12 +0000

    Added support for scale configuration directive to increase scale of iframes.

--- README.md
+++ README.md
@@ -4,10 +4,3 @@ radiator-rotator
 Rotating radiator page. See also:
 
  * [JQuery Cycle Plugin](http://jquery.malsup.com/cycle/)
-
-Future Work
------------
-
-Areas where this project could be improved:
-
- * Add configuratino to ncrease the size of content in an IFRAME.
--- config.js
+++ config.js
@@ -3,6 +3,7 @@ var config = [
  		"url" : "http://10.192.42.156/jenkins/plugin/jenkinswalldisplay/walldisplay.html?viewName=upstream&jenkinsUrl=http://10.192.42.156/jenkins"
 	},
 	{
- 		"url" : "http://10.192.42.156/sonar"
+ 		"url" : "http://10.192.42.156/sonar",
+		"scale" : "1.5"
 	}
 ]
