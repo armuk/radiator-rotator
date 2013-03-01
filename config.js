122ba21e61269d73074c5c8f3e33252626f38989
Ricardo Gladwell <ricardo.gladwell@itv.com>
2013-03-01 10:47:39 +0000

    Added automatic refresh feature for pages that do not support auto-refresh.

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 var config = [
 	{
+		"id" : "jenkins",
  		"url" : "http://10.192.42.156/jenkins/plugin/jenkinswalldisplay/walldisplay.html?viewName=upstream&jenkinsUrl=http://10.192.42.156/jenkins"
 	},
 	{
+		"id" : "sonar",
  		"url" : "http://10.192.42.156/sonar",
-		"scale" : "1.5"
+		"scale" : "1.5",
+		"refresh" : "2000"
 	}
 ]
