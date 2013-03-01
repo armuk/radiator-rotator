8afdbee81f41b38e8866eb364ac3e51c6d317ad2
Ricardo Gladwell <ricardo.gladwell@itv.com>
2013-03-01 10:52:48 +0000

    Added 'pages' sub-property to configuration syntax for better support for future features.

--- config.js
+++ config.js
@@ -1,12 +1,17 @@
-var config = [
-	{
-		"id" : "jenkins",
- 		"url" : "http://10.192.42.156/jenkins/plugin/jenkinswalldisplay/walldisplay.html?viewName=upstream&jenkinsUrl=http://10.192.42.156/jenkins"
-	},
-	{
-		"id" : "sonar",
- 		"url" : "http://10.192.42.156/sonar",
-		"scale" : "1.5",
-		"refresh" : "2000"
-	}
-]
+var SECONDS = 1000
+var MINUTES = 60 * SECONDS
+
+var config = {
+	"pages" : [
+		{
+			"id" : "jenkins",
+ 		   	"url" : "http://10.192.42.156/jenkins/plugin/jenkinswalldisplay/walldisplay.html?viewName=upstream&jenkinsUrl=http://10.192.42.156/jenkins"
+		},
+		{
+			"id" : "sonar",
+ 		   	"url" : "http://10.192.42.156/sonar",
+			"scale" : 1.5,
+			"refresh" : 5 * MINUTES
+		}
+	]
+}
