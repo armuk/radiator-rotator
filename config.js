df8091269a44e5bdefa8f872bb9c5b8fb176552c
Rhys Lloyd Sharrem <rhys.sharrem@gmail.com>
2013-08-23 16:53:49 +0100

    Removed sonar page

--- config.js
+++ config.js
@@ -6,12 +6,6 @@ var config = {
 		{
 			"id" : "jenkins",
  		   	"url" : "http://10.192.42.41/jenkins/plugin/jenkinswalldisplay/walldisplay.html?viewName=upstream&jenkinsUrl=http://10.192.42.41/jenkins"
-		},
-		{
-			"id" : "sonar",
- 		   	"url" : "http://10.192.42.156/sonar",
-			"scale" : 1.5,
-			"refresh" : 5 * MINUTES
 		}
 	]
 }
