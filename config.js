9d74ccdd831ee530a115775504b890a329b72ff9
Rhys Lloyd Sharrem <rhys.sharrem@gmail.com>
2013-08-23 16:34:28 +0100

    Updated IP address for Jenkins box

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = {
 	"pages" : [
 		{
 			"id" : "jenkins",
- 		   	"url" : "http://10.192.42.156/jenkins/plugin/jenkinswalldisplay/walldisplay.html?viewName=upstream&jenkinsUrl=http://10.192.42.156/jenkins"
+ 		   	"url" : "http://10.192.42.41/jenkins/plugin/jenkinswalldisplay/walldisplay.html?viewName=upstream&jenkinsUrl=http://10.192.42.41/jenkins"
 		},
 		{
 			"id" : "sonar",
